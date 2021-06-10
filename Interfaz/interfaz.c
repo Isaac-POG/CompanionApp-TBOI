22bd1b97b96fa66449b982501693743c01500f9e
OkitaDev <soujiok@gmail.com>
2021-06-10 11:54:52 -0400

    Actualización de Menú

--- Interfaz/interfaz.c
+++ Interfaz/interfaz.c
@@ -5,12 +5,21 @@ void mostrarMenuGuardado()
 	printf("\n1.)Partida 1\n");
 	printf("2.)Partida 2\n");
 	printf("3.)Partida 3\n");
+	printf("4.)Eliminar Partida\n");
 	printf("0.)Salir del programa\n");
 	printf("\nElija una opcion: ");
 }
 
 void mostrarMenuOpciones()
 {
-	printf("\n0.)Salir del Menu\n");
+	printf("\n1.)Desbloqueo de Personajes\n");
+	printf("2.)Marcas de Logro\n");
+	printf("3.)Buscar un Item\n");
+	printf("4.)Buscar un Secreto\n");
+	printf("5.)Mostrar todos los Personajes\n");
+	printf("6.)Mostrar todos los Items\n");
+	printf("7.)Mostrar todos los Secretos\n");
+	printf("8.)Mostrar todos los enemigos\n");
+	printf("0.)Salir del Menu\n");
 	printf("\nElija una opcion: ");
 }
